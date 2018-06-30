0a64955e4bef862935c7c0b099eb68c2411b2e6b
David Dulek <ddulek@gmail.com>
2018-06-29 20:19:15 -0500

    added namespace to sonar setup

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -20,17 +20,17 @@ oc rollout status dc/postgresql -n ${GUID}-sonarqube -w
 
 oc new-app -n ${GUID}-sonarqube --docker-image=wkulhanek/sonarqube:6.7.4 --env=SONARQUBE_JDBC_USERNAME=sonar --env=SONARQUBE_JDBC_PASSWORD=sonar --env=SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar --labels=app=sonarqube
 
-oc scale dc/sonarqube --replicas=0
-oc rollout pause dc/sonarqube
+oc scale -n ${GUID}-sonarqube dc/sonarqube --replicas=0
+oc rollout pause -n ${GUID}-sonarqube dc/sonarqube
 
-oc expose service sonarqube
-oc volume dc/sonarqube --add --overwrite --name=sonarqube-volume-1 --mount-path=/opt/sonarqube/data/ --type persistentVolumeClaim --claim-name=sonarqube-pvc --claim-size=4Gi
+oc expose service -n ${GUID}-sonarqube sonarqube
+oc volume -n ${GUID}-sonarqube dc/sonarqube --add --overwrite --name=sonarqube-volume-1 --mount-path=/opt/sonarqube/data/ --type persistentVolumeClaim --claim-name=sonarqube-pvc --claim-size=4Gi
 
-oc set resources dc sonarqube --limits=memory=3Gi,cpu=2 --requests=memory=1.5Gi,cpu=1
-oc patch dc sonarqube --patch='{ "spec": { "strategy": { "type": "Recreate" }}}'
+oc set resources -n ${GUID}-sonarqube dc sonarqube --limits=memory=3Gi,cpu=2 --requests=memory=1.5Gi,cpu=1
+oc patch -n ${GUID}-sonarqube dc sonarqube --patch='{ "spec": { "strategy": { "type": "Recreate" }}}'
 
-oc set probe dc/sonarqube --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok
-oc set probe dc/sonarqube --readiness --failure-threshold 3 --initial-delay-seconds 20 --get-url=http://:9000/about
+oc set probe -n ${GUID}-sonarqube dc/sonarqube --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok
+oc set probe -n ${GUID}-sonarqube dc/sonarqube --readiness --failure-threshold 3 --initial-delay-seconds 20 --get-url=http://:9000/about
 
-oc scale dc/sonarqube --replicas=1
-oc rollout resume dc/sonarqube
+oc scale -n ${GUID}-sonarqube dc/sonarqube --replicas=1
+oc rollout -n ${GUID}-sonarqube resume dc/sonarqube
